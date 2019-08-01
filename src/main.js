0360363203c19cc6f2e5afbe6c4c7b64325f01e0
evaqueen <yy819702005@qq.com>
2019-08-01 16:28:36 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// tip提示信息  [生产环境提示product=false] 即按照开发环境提示  详细一些
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
+// 创建根实例
 new Vue({
+  // render  渲染 app组件
   render: h => h(App)
+  // mount 挂载在#app容器中
 }).$mount('#app')
+
+// main.js的职责
+/*
+1、导入项目需要的依赖的资源（js包，css文件，其他资源）
+2、创建一个根实例
+
+*/
---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">login</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="less">
+// scoped 作用域作用范围
+// style的样式仅仅在当前组件下生效
+// 原理：
+</style>
